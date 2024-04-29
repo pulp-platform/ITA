1a89be3f67867fb83c9656afe4b7768ed5f74caa
Timon Fercho <tfercho@student.ethz.ch>
2024-04-29 13:14:39 +0200

    test(py-ita): add i_gelu tests

--- PyITA/test_ITA.py
+++ PyITA/test_ITA.py
@@ -3,7 +3,31 @@ import torch
 import numpy as np
 import pytest_check as check
 
-from .ITA import i_poly, i_poly_wrapper, quantize, dequantize, i_erf_wrapper
+from .ITA import i_poly, i_poly_wrapper, quantize, dequantize, i_erf_wrapper, i_gelu_wrapper
+
+def test_gelu():
+    xs = np.linspace(0, 1.769, 10)
+    alpha, n_bits = 3, 8
+    x_qs, S = quantize(xs, alpha, n_bits)
+
+    for x, x_q in zip(xs, x_qs):
+        res_q, res_S = i_gelu_wrapper(x_q, S)
+        deq_res = res_q * res_S
+        exp_res = torch.nn.functional.gelu(torch.tensor(x, dtype=torch.float32)).item()
+        print(f"x={x}, x_q={x_q}, S={S}, res_q={res_q}, res_S={res_S}, deq_res={deq_res}, exp_res={exp_res}")
+        check.almost_equal(deq_res, exp_res, abs=1e-1)
+
+def test_gelu_simple():
+    xs = np.array([-20, -10, -3, -2, -1, 0, 1, 2, 3, 10, 20]) * 0.1
+    alpha, n_bits = 3, 8
+    x_qs, S = quantize(xs, alpha, n_bits)
+
+    for x, x_q in zip(xs, x_qs):
+        res_q, res_S = i_gelu_wrapper(x_q, S)
+        deq_res = res_q * res_S
+        exp_res = torch.nn.functional.gelu(torch.tensor(x, dtype=torch.float32)).item()
+        print(f"x={x}, x_q={x_q}, S={S}, res_q={res_q}, res_S={res_S}, deq_res={deq_res}, exp_res={exp_res}")
+        check.almost_equal(deq_res, exp_res, abs=1e-1)
 
 
 def test_erf():
