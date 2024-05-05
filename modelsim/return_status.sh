2cd0b5f5ccfd150a72284573076822affda7e868
Timon Fercho <tfercho@student.ethz.ch>
2024-05-05 23:06:21 +0200

    feat(return-status): adjust parameters and check number of outputs

--- modelsim/return_status.sh
+++ modelsim/return_status.sh
@@ -4,59 +4,73 @@
 # Licensed under the Apache License, Version 2.0, see LICENSE for details.
 # SPDX-License-Identifier: Apache-2.0
 
+export SIM_LOG=$1
+export SEQUENCE_LEN=$2
+export EMBEDDING_SIZE=$3
+export PROJECTION_SIZE=$4
+export FEEDFORWARD_SIZE=$5
+
 # Number of dot product units
 export ITA_N=16
 export ITA_M=64
 
+# Round to the clostest of multiple of $ITA_M
+ita_s=$(( ITA_M * ( (( ${SEQUENCE_LEN} - 1) / ITA_M) + 1) ))
+ita_e=$(( ITA_M * ( (( ${EMBEDDING_SIZE} - 1) / ITA_M) + 1) ))
+ita_p=$(( ITA_M * ( (( ${PROJECTION_SIZE} - 1) / ITA_M) + 1) ))
+ita_f=$(( ITA_M * ( (( ${FEEDFORWARD_SIZE} - 1) / ITA_M) + 1) ))
+
+exp_n_outputs_Q=$((ita_s * ita_p / ITA_N))
+exp_n_outputs_K=${exp_n_outputs_Q}
+exp_n_outputs_V=${exp_n_outputs_Q}
+exp_n_outputs_OW=$((ita_s * ita_e / ITA_N))
+exp_n_outputs_FF1=$((ita_s * ita_f / ITA_N))
+
 # Check if the simulation log exists
-if [ ! -f $1 ]
-then
-    echo "❗ Simulation log not found."
-    exit 1
+if [[ ! -f ${SIM_LOG} ]]; then
+	echo "❗ Simulation log not found."
+	exit 1
 fi
+
+check_n_outputs() {
+	local n_outputs=$1
+	local phase=$2
+
+	if ! grep -q "${n_outputs} outputs were checked in phase ${phase}." "${SIM_LOG}"; then
+		echo "❌ Simulation did not finish successfully. Expected ${n_outputs} outputs in phase ${phase}."
+		exit 1
+	fi
+
+	echo "✅ Checked ${n_outputs} outputs in phase ${phase}."
+}
 
 # ITA TB
 # Check if the simulation log has finished successfully by checking the number of outputs
-if [ $4 == "ita_tb" ]
+if [ ${ENTRY_POINT} == "ita_tb" ]
 then
-    # Round $2 and $3 to the clostest of multiple of $ITA_M
-    ita_s=$(( ITA_M * ( (( $2 - 1) / ITA_M) + 1) ))
-    ita_e=$(( ITA_M * ( (( $3 - 1) / ITA_M) + 1) ))
-    num_outputs=$((ita_s * ita_e / ITA_N))
-    if grep -q "${num_outputs} outputs were checked in phase 4." $1
-    then
-        count=$(grep -c "Wrong value" $1)
+    check_n_outputs "${exp_n_outputs_Q}" 0
+    check_n_outputs "${exp_n_outputs_K}" 1
+    check_n_outputs "${exp_n_outputs_V}" 2
+    check_n_outputs "${exp_n_outputs_OW}" 4
 
-        if [ $count -gt 0 ];
-        then
-            echo "❌ Found ${count} errors in the simulation log."
-            exit 1
-        else
-            echo "✅ No errors found in the simulation log."
-            exit 0
-        fi
-    else
-        echo "❗ Simulation did not finish successfully."
-        exit 1
-    fi
+    n_error_lines=$(grep -c "Wrong value" "${SIM_LOG}")
 # HWPE TB
 # Check if the simulation log has finished successfully
-elif [ $4 == "hwpe_tb" ]
+elif [ ${ENTRY_POINT} == "hwpe_tb" ]
 then
-    if grep -q "Comparing output" $1 && grep -q "\$finish" $1
+    if ! grep -q "Comparing output" $1 && grep -q "\$finish" $1
     then
-        count=$(grep -c "mismatch" $1)
-
-        if [ $count -gt 0 ];
-        then
-            echo "❌ Found ${count} errors in the simulation log."
-            exit 1
-        else
-            echo "✅ No errors found in the simulation log."
-            exit 0
-        fi
-    else
         echo "❗ Simulation did not finish successfully."
         exit 1
     fi
+
+    n_error_lines=$(grep -c "mismatch" "${SIM_LOG}")
 fi
+
+if [[ ${n_error_lines} -gt 0 ]]; then
+	echo "❌ Found ${n_error_lines} errors in the simulation log."
+	exit 1
+fi
+
+echo "✅ No errors found in the simulation log."
+exit 0
