41ca06753247074f7b7107e536cd4e00a6ed2857
Timon Fercho <tfercho@student.ethz.ch>
2024-05-12 22:48:21 +0200

    feat(py-ita): use 8 bit eps_mul for requantizer i-gelu

--- PyITA/ITA.py
+++ PyITA/ITA.py
@@ -850,7 +850,7 @@ def i_gelu(q: i8, q_1: i16, q_b: i16, q_c: i16) -> i32:
     q_out: i32 = q_clipped * (q_erf + q_1)
     return q_out
 
-def requantize(q: i32, eps_mul: i16, eps_shift: i8, eps_add: i8) -> i8:
+def requantize(q: i32, eps_mul: i8, eps_shift: i8, eps_add: i8) -> i8:
     q_mul: i64 = q * eps_mul
     q_req: i8 = round_to_i8(q_mul / 2**eps_shift) + eps_add
     return q_req
@@ -870,13 +870,12 @@ def get_i_gelu_constants(S: f32) -> Tuple[i16, i16, i16, float, float, float]:
     q_c: i16 = round_to_i16(c / (a * S_2**2))
     return q_1, q_b, q_c, a, b, c
 
-def get_i_gelu_requantized_constants(S: f32, D: i32) -> Tuple[i16, i16, i16, float, float, float, i16, i8, i8]:
+def get_i_gelu_requantized_constants(S: f32, D: i32) -> Tuple[i16, i16, i16, float, float, float, i8, i8, i8]:
     q_1, q_b, q_c, a, b, c = get_i_gelu_constants(S)
     S_2: f32 = S / np.sqrt(2)
     S_out: f32 = S * a * S_2**2 / 2
-    eps_mul: i16 = round_to_i16(S_out / S * D)
+    eps_mul: i8 = round_to_i8(S_out / S * D)
     eps_shift: i8 = round_to_i8(np.log2(D))
-    print(f"eps_mul: {eps_mul}, eps_shift: {eps_shift}")
     eps_add: i8 = 0
     return q_1, q_b, q_c, a, b, c, eps_mul, eps_shift, eps_add
 
@@ -889,11 +888,9 @@ def i_gelu_wrapper(q: i8, S: f32) -> Tuple[i32, f32]:
     return q_out, S_out
 
 def i_gelu_wrapper_requantized(q: i8, S: f32, D: i32) -> Tuple[i8, f32]:
-    S_2: f32 = S / np.sqrt(2)
     q_1, q_b, q_c, a, _, _, eps_mul, eps_shift, eps_add = get_i_gelu_requantized_constants(S, D)
     q_out: i32 = i_gelu_reqantized(q, q_1, q_b, q_c, eps_mul, eps_shift, eps_add)
-    S_out: f32 = S
-    return q_out, S_out
+    return q_out, S
 
 def i_erf(q: i8, q_b: i16, q_c: i16) -> i32:
     q_sgn: i8 = np.sign(q)
