87577f54e0fc2f6d067e12e088a5ea828a51876f
Timon Fercho <tfercho@student.ethz.ch>
2024-06-18 10:57:15 +0200

    perf(gelu): apply strength reduction to remove signed mult when computing gelu_erf

--- src/ita_gelu.sv
+++ src/ita_gelu.sv
@@ -14,15 +14,16 @@ module ita_gelu
 
   gelu_out_t data_sign_ext, b_sign_ext;
   gelu_out_t poly_d, poly_sq;
-  gelu_out_t erf_sgn, erf_abs, erf_clipped, erf_L;
+  logic erf_sgn;
+  gelu_out_t erf_abs, erf_clipped, erf_L;
   gelu_out_t gelu_erf, gelu_sum, gelu_out;
 
   always_comb begin
     data_sign_ext = {{GELU_OUT_WIDTH-WI{data_i[WI-1]}}, data_i};
     b_sign_ext = {{GELU_OUT_WIDTH-GELU_CONSTANTS_WIDTH{b_i[GELU_CONSTANTS_WIDTH-1]}}, b_i};
 
-    erf_sgn = data_i < 0 ? -26'd1 : 26'd1;
-    erf_abs = data_i < 0 ? -data_sign_ext : data_sign_ext;
+    erf_sgn = data_i < 0;
+    erf_abs = erf_sgn ? -data_sign_ext : data_sign_ext;
     erf_clipped = erf_abs > -b_sign_ext ? -b_sign_ext : erf_abs;
 
     poly_d = erf_clipped + b_i;
@@ -30,7 +31,7 @@ module ita_gelu
 
     erf_L = poly_sq + c_i;
 
-    gelu_erf = erf_sgn * erf_L;
+    gelu_erf = erf_sgn ? -erf_L : erf_L;
     gelu_sum = gelu_erf + one_i;
     gelu_out = data_i * gelu_sum;
   end
