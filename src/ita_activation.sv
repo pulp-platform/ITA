f3abd65ba8c6aa202cf159d8d0213d09bf685c0f
gamzeisl <gamzeisl@hotmail.com>
2024-09-03 17:07:47 +0200

    feat(activation): fix activation latency to two cycles

--- src/ita_activation.sv
+++ src/ita_activation.sv
@@ -15,11 +15,14 @@ module ita_activation
     input requant_const_t requant_shift_i,
     input requant_const_t requant_add_i,
     input activation_e activation_i,
+    input logic calc_en_i,
+    input logic calc_en_q_i,
     input requant_oup_t  data_i,
     output requant_oup_t data_o
   );
 
-  activation_e activation_q1;
+  requant_oup_t data_q1, data_q2;
+  activation_e activation_q1, activation_q2;
   gelu_out_t [N-1:0] gelu_out;
   requant_oup_t gelu_out_requant;
   requant_oup_t relu_out;
@@ -31,8 +34,8 @@ module ita_activation
     .eps_mult_i(requant_mult_i),
     .right_shift_i(requant_shift_i),
     .add_i({N{requant_add_i}}),
-    .calc_en_i(activation_i === GELU),
-    .calc_en_q_i(activation_q1 === GELU),
+    .calc_en_i(calc_en_i),
+    .calc_en_q_i(calc_en_q_i),
     .result_i(gelu_out),
     .requant_oup_o(gelu_out_requant)
   );
@@ -40,7 +43,7 @@ module ita_activation
   generate
     for (genvar i = 0; i < N; i++) begin: relu_instances
       ita_relu i_relu (
-        .data_i(data_i[i]),
+        .data_i(data_q2[i]),
         .data_o(relu_out[i])
       );
     end
@@ -60,20 +63,26 @@ module ita_activation
 
 
   always_comb begin
-    if (activation_i === GELU) begin
+    if (activation_q2 === GELU) begin
       data_o = gelu_out_requant;
-    end else if (activation_i === RELU) begin
+    end else if (activation_q2 === RELU) begin
       data_o = relu_out;
     end else begin
-      data_o = data_i;
+      data_o = data_q2;
     end
   end
 
-  always_ff @(posedge clk_i or negedge rst_ni) begin
-    if (!rst_ni) begin
+  always_ff @(posedge clk_i) begin
+    if (rst_ni == 0) begin
       activation_q1 <= IDENTITY;
+      activation_q2 <= IDENTITY;
+      data_q1 <= '0;
+      data_q2 <= '0;
     end else begin
       activation_q1 <= activation_i;
+      activation_q2 <= activation_q1;
+      data_q1 <= data_i;
+      data_q2 <= data_q1;
     end
   end
 endmodule
