6260ccda91ba948bfd2a62d3c2718c90cdbba3f6
Marcel Kant <makant@ethz.ch>
2024-10-13 20:12:23 +0200

    Small changes in the debug.py file

--- PyITA/debug.py
+++ PyITA/debug.py
@@ -6,8 +6,9 @@ import matplotlib.pyplot as plt
 
 def print_matrix(from_txt: bool, matrix: np.array = None, 
                                  txt_file: str = 'Out_soft_0.txt', 
-                                 test_vector: str = 'data_S128_E128_P192_F256_H1_B0', 
+                                 test_vector: str = 'data_S30_E30_P50_F64_H1_B0', 
                                  row: int = 128, col: int = 128):
+    
     if (from_txt):
         current_dir = os.path.dirname(os.path.abspath(__file__))
         filepath = os.path.join(os.path.dirname(current_dir),
@@ -23,28 +24,7 @@ def print_matrix(from_txt: bool, matrix: np.array = None,
     sns.heatmap(matrix, annot=False, linewidths=0, linecolor='white', cmap='crest', xticklabels=False, yticklabels=False)
     plt.show()
 
-# row = 128
-# col = 128
-
-# txt_file = 'Out_soft_0.txt'
-# test_vector = 'data_S128_E128_P192_F256_H1_B0'
-
-# current_dir = os.path.dirname(os.path.abspath(__file__))
-# filepath = os.path.join(os.path.dirname(current_dir),
-#                             'simvectors',
-#                             test_vector,
-#                             'standalone',
-#                             txt_file)
-
-# print(filepath)
-# print(os.path.exists(filepath))
-
-# array = np.loadtxt(filepath)
-# matrix = array.reshape(row, col)
-
-# sns.set_theme()
-# sns.heatmap(matrix, annot=False, linewidths=0, linecolor='white', cmap='crest', xticklabels=False, yticklabels=False)
-# plt.show()
+# print_matrix(from_txt=True, txt_file="A_soft_0.txt")
 
 
 
