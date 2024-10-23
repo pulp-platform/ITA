fd7ae8306e122d6b364308b658ae4596720f6404
Marcel Kant <makant@ethz.ch>
2024-10-23 17:13:13 +0200

    Added waves

--- modelsim/sim_ita_tb_wave.tcl
+++ modelsim/sim_ita_tb_wave.tcl
@@ -14,6 +14,29 @@ add wave -noupdate /ita_tb/dut/i_controller/ctrl_i
 add wave -noupdate /ita_tb/dut/oup_o
 add wave -noupdate /ita_tb/dut/inp1_q
 add wave -noupdate /ita_tb/dut/inp2_q
+add wave -noupdate -radix unsigned /ita_tb/dut/i_controller/count_d
+add wave -noupdate -radix unsigned /ita_tb/dut/i_controller/count_q
+add wave -noupdate /ita_tb/dut/calc_en
+add wave -noupdate /ita_tb/dut/calc_en_q1
+add wave -noupdate /ita_tb/dut/calc_en_q2
+add wave -noupdate /ita_tb/dut/calc_en_q3
+add wave -noupdate -expand -group Bias /ita_tb/dut/inp_bias
+add wave -noupdate -expand -group Bias /ita_tb/dut/inp_bias_padded
+add wave -noupdate -expand -group Bias /ita_tb/dut/inp_bias_q1
+add wave -noupdate -expand -group Bias /ita_tb/dut/inp_bias_q2
+add wave -noupdate /ita_tb/dut/i_accumulator/oup_i
+add wave -noupdate /ita_tb/dut/i_accumulator/result_d
+add wave -noupdate /ita_tb/dut/i_accumulator/result_o
+add wave -noupdate /ita_tb/dut/i_requantizer/requant_oup_o
+add wave -noupdate /ita_tb/dut/i_activation/data_i
+add wave -noupdate /ita_tb/dut/i_activation/data_q1
+add wave -noupdate /ita_tb/dut/i_activation/data_q2
+add wave -noupdate /ita_tb/dut/i_activation/data_q3
+add wave -noupdate /ita_tb/dut/i_activation/data_q4
+add wave -noupdate /ita_tb/dut/i_activation/data_o
+add wave -noupdate /ita_tb/dut/i_fifo/data_i
+add wave -noupdate /ita_tb/dut/i_fifo/data_o
+add wave -noupdate /ita_tb/dut/oup_o
 add wave -noupdate -group Requantizer /ita_tb/dut/i_requantizer/*
 add wave -expand -group Controller /ita_tb/dut/i_controller/*
 add wave -group {Softmax Controller} ita_tb/dut/i_softmax_top/i_softmax/*
