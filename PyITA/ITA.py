Only four wrong values for 1 tile

PyITA/ITA.py
@@ -598,6 +598,8 @@ class Transformer:
         else:
             raise ValueError("Mask not supported")
         
+        print(self.Mask)
+        
         matrix = np.squeeze(self.A_requant)
         plt.imshow(matrix, cmap='viridis')
         plt.colorbar()
@@ -689,10 +691,10 @@ class Transformer:
         self.Out_soft_requant = requantize(self.Out_soft, self.requant_eps_mult[5], self.requant_right_shift[5],
                                            self.requant_add[5])
 
-        matrix = np.squeeze(self.Out_soft)
+        matrix = np.squeeze(self.Out_soft_requant)
         plt.imshow(matrix, cmap='viridis')
         plt.colorbar()
-        plt.title("Out_soft")
+        plt.title("Out_soft_requant")
         plt.show()
 
         if (self.S_ITA - self.S) > 0:

PyITA/softmax.py
@@ -129,7 +129,7 @@ def streamingPartialSoftmax(x, mask, integerize = True):
 
         print(f"Global Max: {global_max.shape}")
         print(global_max)
-        print(f"Global Max: {current_max.shape}")
+        print(f"Current Max: {current_max.shape}")
         print(current_max)
 
         # Update all shift values where new maximum is larger
@@ -188,6 +188,10 @@ def streamingPartialSoftmax(x, mask, integerize = True):
 
     # Find the difference between the maximum and x
     diff = np.repeat(global_max, seq_length).reshape(n_heads, seq_length, seq_length) - x.astype(np.int32)
+
+    # The global_max can be smaller than a few positions in x because not all values in x were considered for the global_max due to the mask.
+    # So diff should normally not be smaller than 0
+    diff[mask] = 0
 
     # Shift the values by B-log2B -> multiply by B/2**B = log2e*eps_x
     # Make sure to do use round-half-up instead of round-half-to-even